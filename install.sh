18aa16e2cf7285c17da0dd18023a81e00c96bfde
Killian4096 <killianrush203@gmail.com>
2023-01-21 22:46:46 -0500

    New fix

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-echo "start"
-
 apt-get install -y python3-dev python3-babel python3-venv uwsgi uwsgi-plugin-python3 git build-essential libxslt-dev zlib1g-dev libffi-dev libssl-dev shellcheck
 
 useradd --shell /bin/bash --system --home-dir "/usr/local/searx" searx
 mkdir "/usr/local/searx"
 chown -R "searx:searx" "/usr/local/searx"
 
-echo "start1"
+sudo -H -u searx -i　git clone "https://github.com/searx/searx.git" "/usr/local/searx/searx-src"
 
 sudo -H -u searx -i python3 -m venv "/usr/local/searx/searx-pyenv"
 sudo -H -u searx -i echo ". /usr/local/searx/searx-pyenv/bin/activate" >>  "/usr/local/searx/.profile"
-
-echo "start2"
 
 
 #sudo -H -u searx -i
@@ -22,13 +18,10 @@ sudo -H -u searx -i pip install -U setuptools
 sudo -H -u searx -i pip install -U wheel
 sudo -H -u searx -i pip install -U pyyaml
 
-echo "start3"
 
 cd "/usr/local/searx/searx-src"
 sudo -u searx -i pip install -e .
 #exit
-
-echo "start4"
 
 mkdir -p "/etc/searx"
 
