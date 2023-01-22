aa13eef2f3daf517c3af141b20b91cc1e8f5d865
Killian4096 <killianrush203@gmail.com>
2023-01-22 01:30:18 -0500

    Doing debugging

--- crontab-gen.sh
+++ crontab-gen.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "@reboot sleep 5 && sh -c "\'"/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"\';
 sudo -H -u searx -i sh -c crontab -l | { cat; echo "@reboot sleep 5 && sh -c "\'"/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"\'; } | crontab -
