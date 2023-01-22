666e08a1be8cccd4930ce2019fac279906e21b14
Killian4096 <killianrush203@gmail.com>
2023-01-21 22:16:44 -0500

    Added error check

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 apt-get install -y python3-dev python3-babel python3-venv uwsgi uwsgi-plugin-python3 git build-essential libxslt-dev zlib1g-dev libffi-dev libssl-dev shellcheck
 
