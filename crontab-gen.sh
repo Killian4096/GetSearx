50db853a1960912f0885498dca7701aa20f3d1ba
Killian4096 <killianrush203@gmail.com>
2023-01-22 01:37:05 -0500

    Fixing bug

--- crontab-gen.sh
+++ crontab-gen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "@reboot sleep 5 && sh -c "\'"/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"\';
-sudo -H -u searx -i sh -c crontab -l | { cat; echo "@reboot sleep 5 && sh -c "\'"/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"\'; } | crontab -
+COMMAND="@reboot sleep 5 && sh -c "\'"/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"\'
+echo '$COMMAND'
+sudo -H -u searx -i sh -c "crontab -l | { cat; echo '$COMMAND'; } | crontab -"
