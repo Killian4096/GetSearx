a3ae1e6df74fefe345b0ad82dade84a732e08163
Killian4096 <killianrush203@gmail.com>
2023-01-22 01:22:51 -0500

    Fixing crontab script again

--- crontab-gen.sh
+++ crontab-gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-COMMAND="\'/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py\'"
+COMMAND=\'/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py\'
 echo $COMMAND
 #sudo -H -u searx -i sh -c crontab -l | { cat; echo @reboot sleep 5 && sh -c \'/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py\'; } | crontab -
