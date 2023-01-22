a0a4f0e0ca7d34f999220b0457063f4fb96cb3f5
Killian4096 <killianrush203@gmail.com>
2023-01-22 01:01:12 -0500

    Fixing crontab option

--- crontab-gen.sh
+++ crontab-gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -H -u searx -i sh -c "crontab -l | { cat; echo @reboot sleep 5 && sh -c '/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py'; } | crontab -"
+sudo -H -u searx -i sh -c "crontab -l | { cat; echo "@reboot sleep 5 && sh -c '/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"'; } | crontab -"
