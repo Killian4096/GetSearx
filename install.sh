90889afa136ef830ca0671ca02e765d7cdf10783
Killian4096 <killianrush203@gmail.com>
2023-01-21 22:42:53 -0500

    Fixing

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 echo "start"
 
@@ -11,36 +10,34 @@ chown -R "searx:searx" "/usr/local/searx"
 
 echo "start1"
 
-sudo -H -u searx -i
-python3 -m venv "/usr/local/searx/searx-pyenv"
-echo ". /usr/local/searx/searx-pyenv/bin/activate" >>  "/usr/local/searx/.profile"
-exit
+sudo -H -u searx -i python3 -m venv "/usr/local/searx/searx-pyenv"
+sudo -H -u searx -i echo ". /usr/local/searx/searx-pyenv/bin/activate" >>  "/usr/local/searx/.profile"
 
 echo "start2"
 
 
-sudo -H -u searx -i
-source ~/.profile
-pip install -U pip
-pip install -U setuptools
-pip install -U wheel
-pip install -U pyyaml
+#sudo -H -u searx -i
+sudo -H -u searx -i pip install -U pip
+sudo -H -u searx -i pip install -U setuptools
+sudo -H -u searx -i pip install -U wheel
+sudo -H -u searx -i pip install -U pyyaml
 
 echo "start3"
 
 cd "/usr/local/searx/searx-src"
-pip install -e .
-exit
+sudo -u searx -i pip install -e .
+#exit
 
 echo "start4"
 
 mkdir -p "/etc/searx"
+
 cp "/usr/local/searx/searx-src/utils/templates/etc/searx/use_default_settings.yml" "/etc/searx/settings.yml"
+
 sed -i -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" "/etc/searx/settings.yml"
 sed -i -e "s/{instance_name}/searx@$(uname -n)/g" "/etc/searx/settings.yml"
 
 echo "start5"
 
-cd /usr/local/searx/searx-src
-export SEARX_SETTINGS_PATH="/etc/searx/settings.yml"
-exit
+cd /usr/local/searx/searx-src 
+sudo -u searx -i export SEARX_SETTINGS_PATH="/etc/searx/settings.yml"
