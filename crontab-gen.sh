a42b98959af592970db02ab96af732b03da54d6e
Killian4096 <71559572+Killian4096@users.noreply.github.com>
2023-02-07 19:41:28 +0000

    changed crondelay from 5 to 2

--- crontab-gen.sh
+++ crontab-gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-COMMAND="@reboot sleep 5 && sh -c "\'"/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"\'
+COMMAND="@reboot sleep 2 && sh -c "\'"/usr/local/searx/searx-pyenv/bin/python /usr/local/searx/searx-src/searx/webapp.py"\'
 { crontab -l -u searx; echo $COMMAND; } | crontab -u searx -
